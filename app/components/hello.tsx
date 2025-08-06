12fdb1b9080b104b59f36451da1bb34c5f5b0728
hypergolang <hypergolang@gmail.com>
2025-08-06 13:02:15 +0700

    feat: Display "Hello Next.js" and include client component

---
+++ app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use CLIENT";
+
+function Hello() {
+    console.log("I am a Client Component");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default Hello;
